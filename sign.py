0a1b8ef0877e800636c05f3370fa4482366d79b4
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-01-27 00:14:22 +0800

    Create sign.py

---
+++ sign.py
@@ -0,0 +1,22 @@
+from fastecdsa.curve import secp256k1
+from fastecdsa.keys import export_key, gen_keypair
+
+from fastecdsa import curve, ecdsa, keys, point
+from hashlib import sha256
+
+def sign(m):
+	#generate public key
+	#Your code here
+	keypair=fastecdsa.keys.gen_keypair(SECP256K1)
+	public_key = keypair[1]
+
+	#generate signature
+	#Your code here
+	signature=fastecdsa.ecdsa.sign(m, keypair[0], SECP256K1, hashlib.sha256, False)
+	r = signature[0]
+	s = signature[1]
+
+	assert isinstance( public_key, point.Point )
+	assert isinstance( r, int )
+	assert isinstance( s, int )
+	return( public_key, [r,s] )
